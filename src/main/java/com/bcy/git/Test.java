2677745641712e1de39df5a58a2706123b70feb3
bcy_glb <bcy@163.com>
2021-02-09 16:57:27 +0800

    Git 修改2

--- src/main/java/com/bcy/git/Test.java
+++ src/main/java/com/bcy/git/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Git is Testing");
         System.out.println("Git 进行修改1");
+        System.out.println("Git 进行修改2");
     }
 }
